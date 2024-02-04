551535d924969837dd9bad56dc14103f29bfc119
Khazanov Borys <borya878@gmail.com>
2024-02-04 19:10:21 +0300

    filtering tournaments

--- src/ui/tournaments-filter/tournaments-filter.component.ts
+++ src/ui/tournaments-filter/tournaments-filter.component.ts
@@ -1,13 +1,27 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckboxComponent } from '../checkbox/checkbox.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
 	selector: 'frozen-fantasy-tournaments-filter',
 	standalone: true,
-	imports: [CommonModule, CheckboxComponent],
+	imports: [CommonModule, CheckboxComponent, ReactiveFormsModule],
 	templateUrl: './tournaments-filter.component.html',
 	styleUrl: './tournaments-filter.component.less',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TournamentsFilterComponent {}
+export class TournamentsFilterComponent implements OnInit{
+	form = new FormGroup(
+		{
+			khlLeague: new FormControl<boolean>(true,{nonNullable:true}),
+			nhlLeague: new FormControl<boolean>(true,{nonNullable:true})
+		}
+	);
+	@Output() filterChange = new EventEmitter<Partial<{khlLeague:boolean,nhlLeague:boolean}>>
+	ngOnInit(){
+		this.form.valueChanges.subscribe((value)=>{
+			this.filterChange.emit(value);
+		});
+	}
+}
