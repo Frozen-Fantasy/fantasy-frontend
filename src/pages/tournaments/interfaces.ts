c3c4a501c0506a9427b5f56abf052cdc94ceb22a
Khazanov Borys <borya878@gmail.com>
2024-02-04 18:48:18 +0300

    coins component

--- src/pages/tournaments/interfaces.ts
+++ src/pages/tournaments/interfaces.ts
@@ -1,7 +1,9 @@
+export type HockeyLeague = 'KHL' | 'NHL';
 export interface ITournament {
     name: string;
     startDate: Date;
     participants: number;
     contribution: number;
     reward: number;
+    league: HockeyLeague
 }
--- src/pages/tournaments/tournaments.component.ts
+++ src/pages/tournaments/tournaments.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TournamentCardComponent } from 'src/ui/tournament-card/tournament-card.component';
 import { TournamentsFilterComponent } from 'src/ui/tournaments-filter/tournaments-filter.component';
+import { ITournament } from './interfaces';
 
 @Component({
 	selector: 'frozen-fantasy-tournamnets',
@@ -11,76 +12,86 @@ import { TournamentsFilterComponent } from 'src/ui/tournaments-filter/tournament
 	styleUrl: './tournaments.component.less',
 })
 export class TournamentsComponent {
-	mockTournaments = [
+	mockTournaments:ITournament[] = [
 		{
 			name: 'Турнир 1',
 			startDate: new Date('2022-01-01'),
 			participants: 25,
 			contribution: 450,
-			reward: 1500
+			reward: 1500,
+			league: 'KHL'
 		},
 		{
 			name: 'Турнир 2',
 			startDate: new Date('2022-02-15'),
 			participants: 12,
 			contribution: 200,
-			reward: 900
+			reward: 900,
+			league: 'NHL'
 		},
 		{
 			name: 'Турнир 3',
 			startDate: new Date('2022-03-10'),
 			participants: 18,
 			contribution: 300,
-			reward: 1200
+			reward: 1200,
+			league: 'NHL'
 		},
 		{
 			name: 'Турнир 4',
 			startDate: new Date('2022-04-22'),
 			participants: 20,
 			contribution: 350,
-			reward: 1300
+			reward: 1300,
+			league: 'KHL'
 		},
 		{
 			name: 'Турнир 5',
 			startDate: new Date('2022-05-05'),
 			participants: 15,
 			contribution: 250,
-			reward: 1100
+			reward: 1100,
+			league: 'NHL'
 		},
 		{
 			name: 'Турнир 6',
 			startDate: new Date('2022-06-18'),
 			participants: 30,
 			contribution: 500,
-			reward: 2000
+			reward: 2000,
+			league: 'KHL'
 		},
 		{
 			name: 'Турнир 7',
 			startDate: new Date('2022-07-01'),
 			participants: 12,
 			contribution: 200,
-			reward: 900
+			reward: 900,
+			league: 'KHL'
 		},
 		{
 			name: 'Турнир 8',
 			startDate: new Date('2022-08-13'),
 			participants: 24,
 			contribution: 400,
-			reward: 1400
+			reward: 1400,
+			league: 'NHL'
 		},
 		{
 			name: 'Турнир 9',
 			startDate: new Date('2022-09-28'),
 			participants: 16,
 			contribution: 300,
-			reward: 1200
+			reward: 1200,
+			league: 'NHL'
 		},
 		{
 			name: 'Турнир 10',
 			startDate: new Date('2022-10-15'),
 			participants: 22,
 			contribution: 350,
-			reward: 1300
+			reward: 1300,
+			league: 'KHL'
 		}
 	]
 }
