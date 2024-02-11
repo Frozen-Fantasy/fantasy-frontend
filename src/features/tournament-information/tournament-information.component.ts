545c94f4b31a3c64384e7f9a9afb98ec1c6f4be4
Khazanov Borys <borya878@gmail.com>
2024-02-11 19:26:45 +0300

    add tournaments service

--- src/features/tournament-information/tournament-information.component.ts
+++ src/features/tournament-information/tournament-information.component.ts
@@ -1,12 +1,29 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { ITournament } from 'src/pages/tournaments/interfaces';
+import { TournamentsService } from 'src/services/tournaments.service';
+import { TournamentCardComponent } from 'src/ui/tournament-card/tournament-card.component';
 
 @Component({
 	selector: 'frozen-fantasy-tournament-information',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, TournamentCardComponent],
+	providers: [],
 	templateUrl: './tournament-information.component.html',
 	styleUrl: './tournament-information.component.less',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TournamentInformationComponent {}
+export class TournamentInformationComponent implements OnInit {
+	tournament:ITournament|undefined;
+	@Input('id') id:number = 0;
+
+	constructor(private readonly tournamentsService:TournamentsService){
+
+	}
+	ngOnInit(): void {
+		this.tournament = this.tournamentsService.mockTournaments.find(tournament=>{
+			return tournament.id==this.id;
+		});
+	}
+}
