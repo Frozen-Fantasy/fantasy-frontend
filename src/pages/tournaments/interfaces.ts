6c59791e5942031f25d369c90f871a04e7f256b1
Khazanov Borys <borya878@gmail.com>
2024-02-16 22:36:20 +0300

    tournament information component

--- src/pages/tournaments/interfaces.ts
+++ src/pages/tournaments/interfaces.ts
@@ -1,10 +1,35 @@
 export type HockeyLeague = 'KHL' | 'NHL';
+
+export interface ITeam {
+    name:string;
+}
+
+export interface IMatchResult extends IMatch{
+    homeTeamScore: number;
+    guestTeamScore: number;
+    winner: ITeam;
+}
+
+export interface IMatch{
+    homeTeam: ITeam;
+    guestTeam: ITeam;
+}
+
+export interface ITournamentParticipant{
+    userName: string;
+}
+
 export interface ITournament {
     id: number,
     name: string;
     startDate: Date;
-    participants: number;
+    endDate: Date;
+    participantsCount: number;
     contribution: number;
     reward: number;
-    league: HockeyLeague
+    league: HockeyLeague;
+    teams: ITeam[];
+    schedule: IMatch[];
+    results: IMatchResult[]
+    participants: ITournamentParticipant[]
 }
