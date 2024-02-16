a0d60546c04d8e674618fc890ebad9a6543d14c0
Khazanov Borys <borya878@gmail.com>
2024-02-16 22:36:20 +0300

    tournament information component

--- src/pages/tournaments/mockTournaments.ts
+++ src/pages/tournaments/mockTournaments.ts
@@ -2,93 +2,436 @@ import { ITournament } from "./interfaces";
 
 export const mockTournaments:ITournament[] = [
     {
-        id:1,
-        name: 'Турнир 1',
-        startDate: new Date('2022-01-01'),
-        participants: 25,
-        contribution: 450,
-        reward: 1500,
-        league: 'KHL'
+      id: 1,
+      name: "Ледовое Побоище",
+      startDate: new Date("2025-01-01T00:00:00.000Z"),
+      endDate: new Date("2025-02-01T00:00:00.000Z"),
+      participantsCount: 8,
+      contribution: 50,
+      reward: 200,
+      league: "NHL",
+      teams: [
+        {name: "Boston Bruins"},
+        {name: "Detroit Red Wings"},
+        {name: "Pittsburgh Penguins"},
+        {name: "Ottawa Senators"}
+      ],
+      schedule: [
+        {
+          homeTeam: {name: "Boston Bruins"},
+          guestTeam: {name: "Detroit Red Wings"},
+        },
+        {
+          homeTeam: {name: "Pittsburgh Penguins"},
+          guestTeam: {name: "Ottawa Senators"},
+        }
+      ],
+      results: [
+        {homeTeam: {name: "Boston Bruins"},
+        guestTeam: {name: "Detroit Red Wings"},homeTeamScore: 3, guestTeamScore: 2, winner: {name: "Boston Bruins"}},
+        {homeTeam: {name: "Pittsburgh Penguins"},
+        guestTeam: {name: "Ottawa Senators"},homeTeamScore: 1, guestTeamScore: 4, winner: {name: "Ottawa Senators"}}
+      ],
+      participants:[
+        {
+            userName:'FrostyJoker23'
+        },
+        {
+            userName:'QuantumTiger78'
+        },
+        {
+            userName:'SilverDragon42'
+        }
+      ]
     },
     {
-        id:2,
-        name: 'Турнир 2',
-        startDate: new Date('2022-02-15'),
-        participants: 12,
-        contribution: 200,
-        reward: 900,
-        league: 'NHL'
+      id: 2,
+      name: "Снежная Буря",
+      startDate: new Date("2026-01-01T00:00:00.000Z"),
+      endDate: new Date("2026-02-01T00:00:00.000Z"),
+      participantsCount: 8,
+      contribution: 50,
+      reward: 200,
+      league: "KHL",
+      teams: [
+        {name: "СКА"},
+        {name: "Торпедо"},
+        {name: "Локомотив"},
+        {name: "Северсталь"}
+      ],
+      schedule: [
+        {
+          homeTeam: {name: "СКА"},
+          guestTeam: {name: "Торпедо"},
+        },
+        {
+          homeTeam: {name: "Локомотив"},
+          guestTeam: {name: "Северсталь"},
+        }
+      ],
+      results: [
+        {
+            homeTeam: {name: "СКА"},
+            guestTeam: {name: "Торпедо"}
+          ,homeTeamScore: 4, guestTeamScore: 1, winner: {name: "СКА"}},
+        {homeTeam: {name: "Локомотив"},
+        guestTeam: {name: "Северсталь"},homeTeamScore: 2, guestTeamScore: 3, winner: {name: "Северсталь"}}
+      ],
+      participants:[
+        {
+            userName:'FrostyJoker23'
+        },
+        {
+            userName:'QuantumTiger78'
+        },
+        {
+            userName:'SilverDragon42'
+        }
+      ]
     },
     {
-        id:3,
-        name: 'Турнир 3',
-        startDate: new Date('2022-03-10'),
-        participants: 18,
-        contribution: 300,
-        reward: 1200,
-        league: 'NHL'
-    },
-    {
-        id:4,
-        name: 'Турнир 4',
-        startDate: new Date('2022-04-22'),
-        participants: 20,
-        contribution: 350,
-        reward: 1300,
-        league: 'KHL'
-    },
-    {
-        id:5,
-        name: 'Турнир 5',
-        startDate: new Date('2022-05-05'),
-        participants: 15,
-        contribution: 250,
-        reward: 1100,
-        league: 'NHL'
-    },
-    {
-        id:6,
-        name: 'Турнир 6',
-        startDate: new Date('2022-06-18'),
-        participants: 30,
-        contribution: 500,
-        reward: 2000,
-        league: 'KHL'
-    },
-    {
-        id:7,
-        name: 'Турнир 7',
-        startDate: new Date('2022-07-01'),
-        participants: 12,
-        contribution: 200,
-        reward: 900,
-        league: 'KHL'
-    },
-    {
-        id:8,
-        name: 'Турнир 8',
-        startDate: new Date('2022-08-13'),
-        participants: 24,
-        contribution: 400,
-        reward: 1400,
-        league: 'NHL'
-    },
-    {
-        id:9,
-        name: 'Турнир 9',
-        startDate: new Date('2022-09-28'),
-        participants: 16,
-        contribution: 300,
-        reward: 1200,
-        league: 'NHL'
-    },
-    {
-        id:10,
-        name: 'Турнир 10',
-        startDate: new Date('2022-10-15'),
-        participants: 22,
-        contribution: 350,
-        reward: 1300,
-        league: 'KHL'
-    }
-];
+        id: 3,
+        name: "Морозное Приключение",
+        startDate: new Date("2025-01-01T00:00:00.000Z"),
+        endDate: new Date("2025-02-01T00:00:00.000Z"),
+        participantsCount: 8,
+        contribution: 50,
+        reward: 200,
+        league: "NHL",
+        teams: [
+          {name: "Boston Bruins"},
+          {name: "Detroit Red Wings"},
+          {name: "Pittsburgh Penguins"},
+          {name: "Ottawa Senators"}
+        ],
+        schedule: [
+          {
+            homeTeam: {name: "Boston Bruins"},
+            guestTeam: {name: "Detroit Red Wings"},
+          },
+          {
+            homeTeam: {name: "Pittsburgh Penguins"},
+            guestTeam: {name: "Ottawa Senators"},
+          }
+        ],
+        results: [
+          { homeTeam: {name: "Boston Bruins"},
+          guestTeam: {name: "Detroit Red Wings"},homeTeamScore: 3, guestTeamScore: 2, winner: {name: "Boston Bruins"}},
+          {homeTeam: {name: "Pittsburgh Penguins"},
+          guestTeam: {name: "Ottawa Senators"},homeTeamScore: 1, guestTeamScore: 4, winner: {name: "Ottawa Senators"}}
+        ],
+        participants:[
+            {
+                userName:'FrostyJoker23'
+            },
+            {
+                userName:'QuantumTiger78'
+            },
+            {
+                userName:'SilverDragon42'
+            }
+          ]
+      },
+      {
+        id: 4,
+        name: "Зимний Шторм",
+        startDate: new Date("2026-01-01T00:00:00.000Z"),
+        endDate: new Date("2026-02-01T00:00:00.000Z"),
+        participantsCount: 8,
+        contribution: 50,
+        reward: 200,
+        league: "KHL",
+        teams: [
+          {name: "СКА"},
+          {name: "Торпедо"},
+          {name: "Локомотив"},
+          {name: "Северсталь"}
+        ],
+        schedule: [
+          {
+            homeTeam: {name: "СКА"},
+            guestTeam: {name: "Торпедо"},
+          },
+          {
+            homeTeam: {name: "Локомотив"},
+            guestTeam: {name: "Северсталь"},
+          }
+        ],
+        results: [
+          { homeTeam: {name: "СКА"},
+          guestTeam: {name: "Торпедо"},homeTeamScore: 4, guestTeamScore: 1, winner: {name: "СКА"}},
+          {homeTeam: {name: "Локомотив"},
+          guestTeam: {name: "Северсталь"},homeTeamScore: 2, guestTeamScore: 3, winner: {name: "Северсталь"}}
+        ],
+        participants:[
+            {
+                userName:'FrostyJoker23'
+            },
+            {
+                userName:'QuantumTiger78'
+            },
+            {
+                userName:'SilverDragon42'
+            }
+          ]
+      },
+      {
+        id: 5,
+        name: "Ледяной Фестиваль",
+        startDate: new Date("2025-01-01T00:00:00.000Z"),
+        endDate: new Date("2025-02-01T00:00:00.000Z"),
+        participantsCount: 8,
+        contribution: 50,
+        reward: 200,
+        league: "NHL",
+        teams: [
+          {name: "Boston Bruins"},
+          {name: "Detroit Red Wings"},
+          {name: "Pittsburgh Penguins"},
+          {name: "Ottawa Senators"}
+        ],
+        schedule: [
+          {
+            homeTeam: {name: "Boston Bruins"},
+            guestTeam: {name: "Detroit Red Wings"},
+          },
+          {
+            homeTeam: {name: "Pittsburgh Penguins"},
+            guestTeam: {name: "Ottawa Senators"},
+          }
+        ],
+        results: [
+          {homeTeam: {name: "Boston Bruins"},
+          guestTeam: {name: "Detroit Red Wings"},homeTeamScore: 3, guestTeamScore: 2, winner: {name: "Boston Bruins"}},
+          {homeTeam: {name: "Pittsburgh Penguins"},
+          guestTeam: {name: "Ottawa Senators"},homeTeamScore: 1, guestTeamScore: 4, winner: {name: "Ottawa Senators"}}
+        ],
+        participants:[
+            {
+                userName:'FrostyJoker23'
+            },
+            {
+                userName:'QuantumTiger78'
+            },
+            {
+                userName:'SilverDragon42'
+            }
+          ]
+      },
+      {
+        id: 6,
+        name: "Морозное Противостояние",
+        startDate: new Date("2026-01-01T00:00:00.000Z"),
+        endDate: new Date("2026-02-01T00:00:00.000Z"),
+        participantsCount: 8,
+        contribution: 50,
+        reward: 200,
+        league: "KHL",
+        teams: [
+          {name: "СКА"},
+          {name: "Торпедо"},
+          {name: "Локомотив"},
+          {name: "Северсталь"}
+        ],
+        schedule: [
+          {
+            homeTeam: {name: "СКА"},
+            guestTeam: {name: "Торпедо"},
+          },
+          {
+            homeTeam: {name: "Локомотив"},
+            guestTeam: {name: "Северсталь"},
+          }
+        ],
+        results: [
+          {homeTeam: {name: "СКА"},
+          guestTeam: {name: "Торпедо"},homeTeamScore: 4, guestTeamScore: 1, winner: {name: "СКА"}},
+          {homeTeam: {name: "Локомотив"},
+          guestTeam: {name: "Северсталь"},homeTeamScore: 2, guestTeamScore: 3, winner: {name: "Северсталь"}}
+        ],
+        participants:[
+            {
+                userName:'FrostyJoker23'
+            },
+            {
+                userName:'QuantumTiger78'
+            },
+            {
+                userName:'SilverDragon42'
+            }
+          ]
+      },
+      {
+        id: 7,
+        name: "Зимний Кубок Вихрей",
+        startDate: new Date("2025-01-01T00:00:00.000Z"),
+        endDate: new Date("2025-02-01T00:00:00.000Z"),
+        participantsCount: 8,
+        contribution: 50,
+        reward: 200,
+        league: "NHL",
+        teams: [
+          {name: "Boston Bruins"},
+          {name: "Detroit Red Wings"},
+          {name: "Pittsburgh Penguins"},
+          {name: "Ottawa Senators"}
+        ],
+        schedule: [
+          {
+            homeTeam: {name: "Boston Bruins"},
+            guestTeam: {name: "Detroit Red Wings"},
+          },
+          {
+            homeTeam: {name: "Pittsburgh Penguins"},
+            guestTeam: {name: "Ottawa Senators"},
+          }
+        ],
+        results: [
+          {homeTeam: {name: "Boston Bruins"},
+          guestTeam: {name: "Detroit Red Wings"},homeTeamScore: 3, guestTeamScore: 2, winner: {name: "Boston Bruins"}},
+          {homeTeam: {name: "Pittsburgh Penguins"},
+          guestTeam: {name: "Ottawa Senators"},homeTeamScore: 1, guestTeamScore: 4, winner: {name: "Ottawa Senators"}}
+        ],
+        participants:[
+            {
+                userName:'FrostyJoker23'
+            },
+            {
+                userName:'QuantumTiger78'
+            },
+            {
+                userName:'SilverDragon42'
+            }
+          ]
+      },
+      {
+        id: 8,
+        name: "Ледяные Баталии",
+        startDate: new Date("2026-01-01T00:00:00.000Z"),
+        endDate: new Date("2026-02-01T00:00:00.000Z"),
+        participantsCount: 8,
+        contribution: 50,
+        reward: 200,
+        league: "KHL",
+        teams: [
+          {name: "СКА"},
+          {name: "Торпедо"},
+          {name: "Локомотив"},
+          {name: "Северсталь"}
+        ],
+        schedule: [
+          {
+            homeTeam: {name: "СКА"},
+            guestTeam: {name: "Торпедо"},
+          },
+          {
+            homeTeam: {name: "Локомотив"},
+            guestTeam: {name: "Северсталь"},
+          }
+        ],
+        results: [
+          {homeTeam: {name: "СКА"},
+          guestTeam: {name: "Торпедо"},homeTeamScore: 4, guestTeamScore: 1, winner: {name: "СКА"}},
+          {homeTeam: {name: "Локомотив"},
+          guestTeam: {name: "Северсталь"},homeTeamScore: 2, guestTeamScore: 3, winner: {name: "Северсталь"}}
+        ],
+        participants:[
+            {
+                userName:'FrostyJoker23'
+            },
+            {
+                userName:'QuantumTiger78'
+            },
+            {
+                userName:'SilverDragon42'
+            }
+          ]
+      },
+      {
+        id: 9,
+        name: "Морозные Дуэли",
+        startDate: new Date("2025-01-01T00:00:00.000Z"),
+        endDate: new Date("2025-02-01T00:00:00.000Z"),
+        participantsCount: 8,
+        contribution: 50,
+        reward: 200,
+        league: "NHL",
+        teams: [
+          {name: "Boston Bruins"},
+          {name: "Detroit Red Wings"},
+          {name: "Pittsburgh Penguins"},
+          {name: "Ottawa Senators"}
+        ],
+        schedule: [
+          {
+            homeTeam: {name: "Boston Bruins"},
+            guestTeam: {name: "Detroit Red Wings"},
+          },
+          {
+            homeTeam: {name: "Pittsburgh Penguins"},
+            guestTeam: {name: "Ottawa Senators"},
+          }
+        ],
+        results: [
+          {homeTeam: {name: "Boston Bruins"},
+          guestTeam: {name: "Detroit Red Wings"},homeTeamScore: 3, guestTeamScore: 2, winner: {name: "Boston Bruins"}},
+          {homeTeam: {name: "Pittsburgh Penguins"},
+          guestTeam: {name: "Ottawa Senators"},homeTeamScore: 1, guestTeamScore: 4, winner: {name: "Ottawa Senators"}}
+        ],
+        participants:[
+            {
+                userName:'FrostyJoker23'
+            },
+            {
+                userName:'QuantumTiger78'
+            },
+            {
+                userName:'SilverDragon42'
+            }
+          ]
+      },
+      {
+        id: 10,
+        name: "Замороженные Схватки",
+        startDate: new Date("2026-01-01T00:00:00.000Z"),
+        endDate: new Date("2026-02-01T00:00:00.000Z"),
+        participantsCount: 8,
+        contribution: 50,
+        reward: 200,
+        league: "KHL",
+        teams: [
+          {name: "СКА"},
+          {name: "Торпедо"},
+          {name: "Локомотив"},
+          {name: "Северсталь"}
+        ],
+        schedule: [
+          {
+            homeTeam: {name: "СКА"},
+            guestTeam: {name: "Торпедо"},
+          },
+          {
+            homeTeam: {name: "Локомотив"},
+            guestTeam: {name: "Северсталь"},
+          }
+        ],
+        results: [
+          {homeTeam: {name: "СКА"},
+          guestTeam: {name: "Торпедо"},homeTeamScore: 4, guestTeamScore: 1, winner: {name: "СКА"}},
+          {homeTeam: {name: "Локомотив"},
+          guestTeam: {name: "Северсталь"},homeTeamScore: 2, guestTeamScore: 3, winner: {name: "Северсталь"}}
+        ],
+        participants:[
+            {
+                userName:'FrostyJoker23'
+            },
+            {
+                userName:'QuantumTiger78'
+            },
+            {
+                userName:'SilverDragon42'
+            }
+          ]
+      },
+  ]
+  ;
