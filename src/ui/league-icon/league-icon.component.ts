b2d2bc7ae4f90d08a2fa62609b5d2b21a3ea2e8b
Khazanov Borys <borya878@gmail.com>
2024-02-17 19:37:18 +0300

    match-results table

---
+++ src/ui/league-icon/league-icon.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HockeyLeague } from 'src/pages/tournaments/interfaces';
+export type iconSize = 's' | 'm' | 'l';
+@Component({
+	selector: 'frozen-fantasy-league-icon',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './league-icon.component.html',
+	styleUrl: './league-icon.component.less',
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LeagueIconComponent {
+	@Input() league:HockeyLeague | undefined;
+	@Input() size:iconSize = 'm';
+}
--- src/ui/tournament-card/tournament-card.component.ts
+++ src/ui/tournament-card/tournament-card.component.ts
@@ -4,12 +4,12 @@ import { ITournament } from 'src/pages/tournaments/interfaces';
 import { CoinsComponent } from '../kit/coins/coins.component';
 import { ButtonComponent } from '../kit/button/button.component';
 import { Router, RouterLink } from '@angular/router';
-import { LeagueIconComponent } from '../kit/league-icon/league-icon.component';
+import { LeagueIconComponent } from '../league-icon/league-icon.component';
 
 @Component({
 	selector: 'frozen-fantasy-tournament-card',
 	standalone: true,
-	imports: [CommonModule, CoinsComponent, ButtonComponent, RouterLink, LeagueIconComponent],
+	imports: [CommonModule, CoinsComponent, ButtonComponent, RouterLink],
 	templateUrl: './tournament-card.component.html',
 	styleUrl: './tournament-card.component.less',
 	changeDetection: ChangeDetectionStrategy.OnPush,
