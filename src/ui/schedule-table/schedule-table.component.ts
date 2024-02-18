e6d46283325a094328c1018504657732e06ce5cf
Khazanov Borys <borya878@gmail.com>
2024-02-18 18:56:02 +0300

    schedule component

---
+++ src/ui/schedule-table/schedule-table.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IMatch } from 'src/pages/tournaments/interfaces';
+import { TeamComponent } from '../kit/team/team.component';
+import { MatchResultComponent } from '../match-result/match-result.component';
+
+@Component({
+	selector: 'frozen-fantasy-schedule-table',
+	standalone: true,
+	imports: [CommonModule,TeamComponent,MatchResultComponent],
+	templateUrl: './schedule-table.component.html',
+	styleUrl: './schedule-table.component.less',
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ScheduleTableComponent {
+	@Input() matches: IMatch[] = [];
+}
