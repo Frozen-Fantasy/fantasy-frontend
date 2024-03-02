9c9bd1a4f1ba9b056112a9ecbb326ab944513dc8
Khazanov Borys <borya878@gmail.com>
2024-03-02 17:13:33 +0300

    input component

--- src/pages/login/login.component.ts
+++ src/pages/login/login.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InputComponent } from 'src/ui/kit/input/input.component';
 
 @Component({
 	selector: 'frozen-fantasy-login',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, InputComponent],
 	templateUrl: './login.component.html',
 	styleUrl: './login.component.less',
 	changeDetection: ChangeDetectionStrategy.OnPush,
---
+++ src/ui/kit/input/input.component.ts
@@ -0,0 +1,30 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ControlValueAccessor } from '@angular/forms';
+
+@Component({
+	selector: 'frozen-fantasy-input',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './input.component.html',
+	styleUrl: './input.component.less',
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class InputComponent implements ControlValueAccessor {
+	value:string = '';
+	@Input() isPassword:boolean=false;
+	onChange = (_:any) => {};
+	onBlur = (_:any) => {};
+
+	writeValue(value: string): void {
+		this.value = value;
+	}
+
+	registerOnChange(fn: any): void {
+		this.onChange = fn;
+	}
+
+	registerOnTouched(fn: any): void {
+		this.onBlur = fn;
+	}
+}
