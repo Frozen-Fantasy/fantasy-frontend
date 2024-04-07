df7559a6f997d32691712e89359f99bf5743199e
KhazanovBorys <borya878@gmail.com>
2024-04-07 19:46:36 +0300

    fix refresh token logic and add logout logic

--- src/core/core.component.ts
+++ src/core/core.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnDestroy, importProvidersFrom } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, provideRouter } from '@angular/router';
+import { Router, RouterModule, provideRouter } from '@angular/router';
 import { coreRoutes } from './core.routes';
 import { UserService } from 'src/services/auth/user.service';
-import { Subject, takeUntil } from 'rxjs';
+import { Subject, take, takeUntil } from 'rxjs';
+import { AuthService } from 'src/services/auth/auth.service';
 
 @Component({
 	selector: 'frozen-fantasy-core',
@@ -13,14 +14,19 @@ import { Subject, takeUntil } from 'rxjs';
 	templateUrl: './core.component.html',
 	styleUrl: './core.component.less',
 })
-export class CoreComponent implements OnDestroy{
+export class CoreComponent implements OnDestroy {
 	destroy$ = new Subject();
-	constructor(private usersService:UserService){
-		this.usersService.getUserInfo().pipe(takeUntil(this.destroy$)).subscribe((val)=>{
+	constructor(private usersService: UserService, private authService: AuthService, private router: Router) {
+		this.usersService.getUserInfo().pipe(takeUntil(this.destroy$)).subscribe((val) => {
 			console.log(val);
 		});
 	}
 	ngOnDestroy(): void {
 		this.destroy$.next(true);
 	}
- }
+	onLogout(): void {
+		this.authService.logout().pipe(take(1)).subscribe(() => {
+			this.router.navigate(['/login']);
+		});
+	}
+}
