392140137e0e6ea1a05088396113b69448afe12a
KhazanovBorys <borya878@gmail.com>
2024-04-14 20:10:38 +0300

    buy product api

--- src/ui/product-card/product-card.component.ts
+++ src/ui/product-card/product-card.component.ts
@@ -1,16 +1,22 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IProduct } from 'src/pages/shop/interfaces';
 import { CoinsComponent } from '../kit/coins/coins.component';
+import { ButtonComponent } from '../kit/button/button.component';
 
 @Component({
 	selector: 'frozen-fantasy-product-card',
 	standalone: true,
-	imports: [CommonModule, CoinsComponent],
+	imports: [CommonModule, CoinsComponent, ButtonComponent],
 	templateUrl: './product-card.component.html',
 	styleUrl: './product-card.component.less',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductCardComponent {
 	@Input() product!: IProduct;
+	@Output() buy = new EventEmitter<number>();
+
+	onBuy(id: number) {
+		this.buy.emit(id);
+	}
 }
