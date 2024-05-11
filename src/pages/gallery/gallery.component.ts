43f670160c30a7f2dbb846cdea94c1f79a56e116
KhazanovBorys <borya878@gmail.com>
2024-05-11 18:22:53 +0300

    gallery filters

--- src/pages/gallery/gallery.component.ts
+++ src/pages/gallery/gallery.component.ts
@@ -2,25 +2,43 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlayerCardComponent } from 'src/ui/player-card/player-card.component';
 import { GalleryService } from './gallery.service';
-import { take, tap } from 'rxjs';
+import { filter, take, tap } from 'rxjs';
 import { LetDirective } from 'src/utils/directives/ngLet.directive';
 import { ButtonComponent } from 'src/ui/kit/button/button.component';
 import { TabsComponent } from 'src/ui/kit/tabs/tabs.component';
 import { TabComponent } from 'src/ui/kit/tab/tab.component';
+import { CheckboxComponent } from 'src/ui/kit/checkbox/checkbox.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { InputComponent } from 'src/ui/kit/input/input.component';
+import { IPlayer } from './interfaces';
 
 @Component({
 	selector: 'frozen-fantasy-gallery',
 	standalone: true,
-	imports: [CommonModule, PlayerCardComponent, LetDirective, TabsComponent, TabComponent, ButtonComponent],
+	imports: [CommonModule, PlayerCardComponent, LetDirective, TabsComponent, TabComponent, ButtonComponent, CheckboxComponent, ReactiveFormsModule, InputComponent],
 	templateUrl: './gallery.component.html',
 	styleUrl: './gallery.component.less',
 })
 export class GalleryComponent {
 	unpackedPlayers$ = this.galleryService.unpackedPlayers$;
 	packedPlayers$ = this.galleryService.packedPlayers$;
+	initialUnpackedPlayers: IPlayer[] = [];
+	form = new FormGroup({
+		position: new FormControl(''),
+		name: new FormControl(''),
+		khlLeague: new FormControl<boolean>(true, { nonNullable: true }),
+		nhlLeague: new FormControl<boolean>(true, { nonNullable: true })
+	})
 	constructor(private galleryService: GalleryService) {
 		this.galleryService.getUnpackedPlayers();
 		this.galleryService.getPackedPlayers();
+		this.galleryService.unpackedPlayers$.pipe(filter(players => !!players.length), take(1)).subscribe((players) => {
+			console.log(players);
+			this.initialUnpackedPlayers = players
+		});
+		this.form.valueChanges.subscribe(formValue => {
+			this.filterPlayers();
+		})
 	}
 
 	unpackPlayer(id: number) {
@@ -38,5 +56,22 @@ export class GalleryComponent {
 		this.packedPlayers$.pipe(take(1)).subscribe(players => {
 			players.filter(player => !player.unpacked).forEach(player => this.unpackPlayer(player.id));
 		});
+	}
+
+	filterPlayers() {
+		const filteredByLeague = this.initialUnpackedPlayers.filter(player => {
+			if (player.leagueName === 'NHL') {
+				return this.form.get('nhlLeague')?.value;
+			}
+			if (player.leagueName === 'KHL') {
+				return this.form.get('khlLeague')?.value;
+			}
+			return false;
+		});
+
+		const filteredByName = filteredByLeague.filter(player => {
+			return player.name.toLowerCase().trim().includes(this.form.get('name')?.value?.trim() ?? '');
+		});
+		this.galleryService.unpackedPlayers$.next(filteredByName);
 	}
 }
